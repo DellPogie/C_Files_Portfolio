9840d2360f0401735e24f5e9b78b76d184ba9e1d
Delio Labaddan <149133144+DellPogie@users.noreply.github.com>
2023-12-02 14:19:36 +0800

    Add files via upload

---
+++ C-Hello_World.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("ooooo   ooooo           oooo  oooo                   oooooo   oooooo     oooo                    oooo        .o8  .o.\n`888'   `888'           `888  `888                    `888.    `888.     .8'                     `888       "" 888  888\n 888     888   .ooooo.   888   888   .ooooo.           `888.   .8888.   .8'    .ooooo.  oooo d8b  888   .oooo888  888\n 888ooooo888  d88' `88b  888   888  d88' `88b           `888  .8'`888. .8'    d88' `88b `888""""8P    888  d88' `888  Y8P\n 888     888  888ooo888  888   888  888   888            `888.8'  `888.8'     888   888  888      888  888   888  `8'\n 888     888  888    .o  888   888  888   888 .o.         `888'    `888'      888   888  888      888  888   888  .o.\no888o   o888o `Y8bod8P' o888o o888o `Y8bod8P' Y8P          `8'      `8'       `Y8bod8P' d888b    o888o `Y8bod88P""  Y8P ");
+	return 0;
+}
